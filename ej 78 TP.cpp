da26f687ec45f939711402a379ac9d05e3c3216d
GabrielConforte <82458582+GabrielConforte@users.noreply.github.com>
2022-08-25 13:44:01 -0300

    Update ej 78 TP.cpp

--- ej 78 TP.cpp
+++ ej 78 TP.cpp
@@ -11,9 +11,6 @@ struct tprod {
 	int stk;
 };
 
-//buscar manera de detectar codigos repetidos
-
-
 //este metodo sirve para crear el archivo que se leera durante el ejercicio
 void Generararchivo() {
 	tprod rprod;
@@ -60,17 +57,17 @@ void LeerArchivo (tprod VP[30], int &N) {
 };
 
 void Pedidos(tprod VP[30], int &N){
+
 	int codp,cant;
 	cout<<"Ingrese codigo de producto (0 para cortar): "<<endl;
 	cin>>codp;
-	int size = N;
 	bool flag = false;
 	while (codp!=0){
 		cout<<"Ingrese cantidad: "<<endl;
 		cin>>cant;
 		//busca dentro del array el producto con el codigo
-		for (int i = 0; i < size; ++i) {
-        		if(VP[i].codp==codp){//ve si el codigo ingresado coincide con el de alguno de los productos.
+		for (int i = 0; i < N; ++i) {
+        		if(VP[i].codp==codp && flag == false){//ve si el codigo ingresado coincide con el de alguno de los productos.
 					if(VP[i].stk>=cant){//revisa si el stock es mayor o igual que la cantidad.
 						VP[i].stk=VP[i].stk-cant;//si todo esta bien, resta la cantidad al stock
 					}else{
@@ -78,14 +75,31 @@ void Pedidos(tprod VP[30], int &N){
 					}
 					flag = true;
 				}
-				if(flag == false){//en caso de no encontrarse nunca el codigo se activa este if para avisar que no se encuentra
-					cout << "no se encuentra el codigo"<<endl;
-				}
+		}
+		if(flag){
+			flag=false;
+		} else {
+			cout<<"No se encontro el codigo"<<endl;
 		}
 		cout<<"Ingrese codigo de producto (0 para cortar): "<<endl;
 		cin>>codp;
 		}
+
 }
+
+void ActualizarProd(tprod VP[], int&n){
+    int j;
+    FILE *aprod;
+    tprod rprod;
+    aprod=fopen("listaprod.dat","r+b");
+
+    for (j=0 ; j<=n; j++) {
+    	rprod = VP[j];
+    	fwrite(&rprod, sizeof(rprod), 1, aprod);
+    }
+    fclose(aprod);
+}
+
 
 int main () {
    int opc = -1;
@@ -94,7 +108,8 @@ int main () {
    do {
       cout<<"Ingrese la opcion deseada"<<endl;
       cout<<"1) Generar archivo"<<endl;
-	  cout<<"2) Ejercicio 78"<<endl;
+	  cout<<"2) Leer Archivo"<<endl;
+	  cout<<"3) Ejercicio 78"<<endl;
       cout<<"0) Salir"<<endl;
       cin >> opc;
 
@@ -106,10 +121,11 @@ int main () {
             Generararchivo();//generamos el archivo
             break;
          case 2:
-            LeerArchivo(VP, N);//leemos el archivo y cargamos todo a VP, tambien obtenemos N
-			Pedidos(VP, N);
+            LeerArchivo(VP, N);
 			break;
 		case 3:
+			Pedidos(VP, N);
+			ActualizarProd(VP, N);
             break;
             cout <<"ERROR! La opcion seleccionada es invalida"<<endl;
             break;
